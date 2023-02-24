abcbaa12e04584a00e357effb2a9f458287ced21
Safi100 <safinafi12@gmail.com>
2023-02-24 17:36:57 +0200

    finish responsive header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react'
-
+import Header from './components/header/Header'
+import './app.css'
 const App = () => {
     return (
-        <div>
-            
-        </div>
+        <>
+            <Header />            
+        </>
     )
 }
 
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -8,6 +8,14 @@ const Header = () => {
             <img className='top_header' src={Header_img} alt="top_Header_img" />
             <div className="wrapper">
                 <Nav />
+                <div className="header_info">
+                    <h2>Managing freelance payments has never been easier</h2>
+                    <p>End-to-end payments and financial management in a single solution. Meet the right platform to help realize.</p>
+                    <div className="buttons">
+                        <button className='button get_started_btn'>Get Started</button>
+                        <button className='button request_demo_btn'>Request Demo</button>
+                    </div>
+                </div>
             </div>
         </div>
     );
