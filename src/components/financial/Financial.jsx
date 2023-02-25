a50aa7ac7483f4f729aa3cf8e0193efeb0f64bd5
Safi100 <safinafi12@gmail.com>
2023-02-25 21:36:12 +0200

    setup financial jsx

---
+++ src/components/financial/Financial.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Icon from '../../assets/icon-financial.png'
+import Icon2 from '../../assets/icon-2-financial.png'
+import Icon3 from '../../assets/icon-3-financial.png'
+import './financial.css'
+const Financial = () => {
+    return (
+        <div className='financial'>
+            <div className="wrapper">
+                
+            </div>
+        </div>
+    );
+}
+
+export default Financial;
