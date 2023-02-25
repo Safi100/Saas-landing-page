cbc3d5999a0e093dcc48d2c7d7df2756a2730d7b
Safi100 <safinafi12@gmail.com>
2023-02-25 22:17:08 +0200

    working on finiancial component...

--- src/components/financial/Financial.jsx
+++ src/components/financial/Financial.jsx
@@ -2,12 +2,18 @@ import React from 'react';
 import Icon from '../../assets/icon-financial.png'
 import Icon2 from '../../assets/icon-2-financial.png'
 import Icon3 from '../../assets/icon-3-financial.png'
+import Image_financial from '../../assets/Image_financial.png'
 import './financial.css'
 const Financial = () => {
     return (
         <div className='financial'>
             <div className="wrapper">
-                
+                <div className='image_div'><img src={Image_financial} alt="Image_financial" /></div>
+                <div className="financial_info">
+                    <p className='choose_us_text'>Why Choose Us</p>
+                    <h2>Specialist aiding clients on financial challenges</h2>
+                    <p className='financial_body'>End-to-end payments and financial management in a single solution. Meet the right platform to help realize.</p>
+                </div>
             </div>
         </div>
     );
