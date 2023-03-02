34516b24a4dcf991c8d29513fd6b495dec982fef
Safi100 <safinafi12@gmail.com>
2023-03-02 13:46:47 +0200

    prevent  submit form

--- src/components/signUp/form.jsx
+++ src/components/signUp/form.jsx
@@ -1,20 +1,23 @@
 import React from 'react';
 import formElement from '../../assets/form_element.png'
 const Form = () => {
+    const preventSubmit = (e) => {
+        e.preventDefault()
+    }
     return (
-        <form className='sign_form'>
+        <form className='sign_form' onSubmit={(e)=> preventSubmit(e)}>
             <img className='form_element' src={formElement} alt="formElement" />
             <div>
                 <label htmlFor="">NAME:</label>
-                <input type="text" placeholder='Full Name' />
+                <input type="text" placeholder='Full Name' required/>
             </div>
             <div>
                 <label htmlFor="">EMAIL:</label>
-                <input type="email" placeholder='Your Email Address' />
+                <input type="email" placeholder='Your Email Address' required/>
             </div>
             <div>
                 <label htmlFor="">PASSWORD:</label>
-                <input type="text" placeholder='****************' />
+                <input type="text" placeholder='****************' required/>
             </div>
             <div>
                 <button className='button'>Sign Up</button>
